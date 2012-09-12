fb7c3160ada5e0b8b80fa7634ca5c2f7415f88ca
Eriks Remess <eriks@remess.lv>
2012-09-12 04:27:24 +0300

    First, probably buggy, version of GeekIt for Safari #gklst

---
+++ Geekit for Safari.safariextension/js/bookmarklet.js
@@ -0,0 +1,22 @@
+safari.application.addEventListener("popover", function(){
+	var iframes = document.getElementsByTagName('iframe')
+	if(typeof iframes != "undefined"){
+		for(var i = iframes.length; i-- > 0;){
+			var iframe = iframes[i]
+			iframe.parentNode.removeChild(iframe)
+		}
+	}
+	try {
+		document.getElementsByTagName('img')[0].style.display = "block"
+		var tab = safari.application.activeBrowserWindow.activeTab
+		document.body.innerHTML += '<iframe src="https://geekli.st/link/modal/?url='+escape(tab.url)+'&title='+escape(tab.title)+'" scrolling="no">Please Enable iFrames.</iframe>'
+		document.getElementsByTagName('iframe')[0].addEventListener('load', function(){
+			document.getElementsByTagName('iframe')[0].className += " loaded"
+			document.getElementsByTagName('img')[0].style.display = "none"
+			document.body.className += " loaded"
+		})
+	} catch(e){}
+	document.getElementsByTagName('a')[0].addEventListener('click', function(){
+			safari.self.hide()
+	})
+}, true)
